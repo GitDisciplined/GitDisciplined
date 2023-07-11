908bebb458832a41bcaff16708d0a4f75ce63132
GitDisciplined <68583436+GitDisciplined@users.noreply.github.com>
2023-07-11 15:10:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function showTab(tabName) {
+  var tabButtons = document.getElementsByClassName("tab");
+  for (var i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove("active");
+  }
+  
   var tabContent = document.getElementsByClassName("tab-content");
   for (var i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = "none";
   }
+
   document.getElementById(tabName).style.display = "block";
+  document.querySelector(".tab[data-tab='" + tabName + "']").classList.add("active");
 }

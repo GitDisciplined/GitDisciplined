9e18ebbe1884765c15544371dabdf56be0445fb1
GitDisciplined <68583436+GitDisciplined@users.noreply.github.com>
2023-07-11 15:02:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showTab(tabName) {
+  var tabContent = document.getElementsByClassName("tab-content");
+  for (var i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
